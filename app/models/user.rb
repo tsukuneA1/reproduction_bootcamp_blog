76cd096b0053569bf959bf72c46799db6c4250a2
tsukuneA1 <tkschr1125@gmail.com>
2025-05-11 17:42:53 +0900

    delete:要らないrelation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,6 @@ class User < ApplicationRecord
     has_many :blogs, foreign_key: :author_id, inverse_of: :author, dependent: :destroy
 
     has_many :bookmarks, dependent: :destroy
-    has_many :bookmarked_blogs, through: :bookmarks, source: :blog
 
     has_many :followee_relationships, class_name: "Relationship", foreign_key: :follower_id, dependent: :destroy
     has_many :followees, through: :followee_relationships, source: :followee
